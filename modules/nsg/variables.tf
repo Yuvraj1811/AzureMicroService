505004988fb24d91c6198d321e47f1f5f1eef818
Yuvraj1811 <ys2029472@gmail.com>
2025-11-12 21:22:02 +0530

    change code of security rules and subnet_id

--- modules/nsg/variables.tf
+++ modules/nsg/variables.tf
@@ -1,6 +1,6 @@
 variable "nsg_name" {
-    type = string
-  
+  type = string
+
 }
 
 variable "rg_name" {
@@ -12,22 +12,22 @@ variable "location" {
 }
 
 variable "security_rules" {
-    type = map(list(object({
-      name = string
-      priority = number
-      direction = string
-      access = string
-      protocol = string
-      source_port_range = string
-      destination_port_range = string
-      source_address_prefix = string
-      destination_address_prefix = string
-    })))
+  type = list(object({
+    name                       = string
+    priority                   = number
+    direction                  = string
+    access                     = string
+    protocol                   = string
+    source_port_range          = string
+    destination_port_range     = string
+    source_address_prefix      = string
+    destination_address_prefix = string
+  }))
 
-    default = {}
-  
+  default = {}
+
 }
 
 variable "subnet_id" {
-    type = map(string)
+  type = string
 }
